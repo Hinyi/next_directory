6046076e55fa26487b2c47a551c2e0d5777ae805
Hinyi <mrmck16@gmail.com>
2024-12-21 21:25:46 +0100

    Added search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,20 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">Pitch your StartUp!</h1>
 
         <p className="sub-heading !max-w-3xl">Submit Ideas</p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
