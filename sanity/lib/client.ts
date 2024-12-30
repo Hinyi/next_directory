0db233077c273ae7339ba7514ea2220a0911b977
Hinyi <mrmck16@gmail.com>
2024-12-30 23:48:11 +0100

    More changes

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -5,8 +5,6 @@ import { apiVersion, dataset, projectId } from '../env'
 export const client = createClient({
   projectId,
   dataset,
-  apiVersion,
+  apiVersion, // 
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  
-  
 })
