d5ca92619e7eb7c8f67ca253c0f339e529a2af18
Hinyi <mrmck16@gmail.com>
2025-01-04 21:10:02 +0100

    Updated queries

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -16,3 +16,23 @@ export const STARTUPS_QUERY = defineQuery(
         _createdAt,
         }`
 );
+
+export const STARTUPS_BY_ID_QUERY= defineQuery(`*[_type == "startup" && _id == $id][0]{
+        _id,
+        title,
+        slug,
+        author->{
+            name,
+            image,
+            image,
+            username,
+            bio
+            },
+        views,
+        description,
+        category,
+        image,
+        _createdAt,
+        pitch
+          }`
+);
